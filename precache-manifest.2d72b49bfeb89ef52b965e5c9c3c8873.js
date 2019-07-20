0283114c426dbeb3bb4ab0376babcd321576d0a9
berngarciamadrid <bernabemadrid@gmail.com>
2019-07-21 00:44:22 +0200

    Updates

---
+++ precache-manifest.2d72b49bfeb89ef52b965e5c9c3c8873.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a46446a429975e489005669d074fe8",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "26def9319ff67493acd3",
+    "url": "/monsters-rolodex-react/static/css/main.fe9f2d1e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex-react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "26def9319ff67493acd3",
+    "url": "/monsters-rolodex-react/static/js/main.34a77347.chunk.js"
+  },
+  {
+    "revision": "590cdc23d4720e222623",
+    "url": "/monsters-rolodex-react/static/js/runtime~main.56d562c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
